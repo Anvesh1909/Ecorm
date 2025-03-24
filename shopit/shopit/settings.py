f432b31c3715fa2573d85d3b5ac1fe2dd8c7732f
Anvesh <143001765+Anvesh1909@users.noreply.github.com>
2025-03-25 03:02:56 +0530

    update

--- shopit/shopit/settings.py
+++ shopit/shopit/settings.py
@@ -94,23 +94,23 @@ WSGI_APPLICATION = 'shopit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shopit',
-        'USER': 'root',
-        'PASSWORD': 'Anv@14078',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'shopit',
+#         'USER': 'root',
+#         'PASSWORD': 'Anv@14078',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
