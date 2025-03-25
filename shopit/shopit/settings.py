c576def22e3ef3bdc7adc6afaf107a8cacf90283
Anvesh <143001765+Anvesh1909@users.noreply.github.com>
2025-03-25 11:00:36 +0530

    Update settings.py

--- shopit/shopit/settings.py
+++ shopit/shopit/settings.py
@@ -153,6 +153,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = 'img/'
 MEDIA_ROOT = BASE_DIR/"media"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
