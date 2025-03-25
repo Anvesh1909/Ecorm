b83058d23a0f2e703d63feec934693bfc082c509
Anvesh <143001765+Anvesh1909@users.noreply.github.com>
2025-03-25 11:29:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,15 @@
 # exit on error
 set -o errexit
 
-echo "Installing dependencies..."
+# Install python dependencies
 pip install -r requirements.txt
 
-echo "Navigating to Django project directory..."
+# Navigate to the Django project directory
 cd shopit
 
-echo "Making migrations for core app..."
-python manage.py makemigrations core
-
-echo "Making migrations for shop_app..."
-python manage.py makemigrations shop_app
-
-echo "Running migrations..."
-python manage.py migrate core
-python manage.py migrate shop_app
+# Run migrations
+python manage.py makemigrations
 python manage.py migrate
 
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
-
-echo "Build completed!" 
+# Collect static files
+python manage.py collectstatic --no-input 
